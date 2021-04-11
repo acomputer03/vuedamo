c89a481561a2989866c8ebb68f2ff7fd62af336b
YI-RU <acomputer03@gmail.com>
2021-04-11 21:36:47 +0800

    第一次上傳

---
+++ src/customerpages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h5>我是首頁我還沒做</h5>
+    </div>
+</template>
---
+++ src/customerpages/Layout.vue
@@ -0,0 +1,26 @@
+  
+<template>
+  <div>
+    <UserHeader></UserHeader>
+    <router-view></router-view>
+    <CartIcon></CartIcon>
+    <!-- <UserFooter></UserFooter> -->
+  </div>
+</template>
+<script>
+import UserHeader from "@/customerpages/components/UserHeader";
+// import UserFooter from "@/components/User/UserFooter";
+ import CartIcon from "@/customerpages/components/CartIcon";
+// import Alert from "@/components/Alert";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    UserHeader,
+    // UserFooter,
+    CartIcon,
+    // Alert
+  }
+};
+</script>
